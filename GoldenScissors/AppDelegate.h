69c6fd531bd5480a689dd5fb3a807b3ce77fe729
EventHorizon <crowledj@EventHorizons-MacBook-Pro.local>
2014-12-15 16:16:51 +0000

    Initial Commit

---
+++ GoldenScissors/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoldenScissors
+//
+//  Created by EventHorizon on 15/12/2014.
+//  Copyright (c) 2014 EventHorizon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoldenScissorsTests/GoldenScissorsTests.m
@@ -0,0 +1,34 @@
+//
+//  GoldenScissorsTests.m
+//  GoldenScissorsTests
+//
+//  Created by EventHorizon on 15/12/2014.
+//  Copyright (c) 2014 EventHorizon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoldenScissorsTests : XCTestCase
+
+@end
+
+@implementation GoldenScissorsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
